d6190b6fd28c152cb0c7b5d92414d9c9934b7500
VirgilWei227 <MF1923077@smail.nju.edu.cn>
2024-06-08 01:04:28 +0800

    0608

--- python_code/stream.py
+++ python_code/stream.py
@@ -3,17 +3,49 @@ def sieve(stream):
     yield prime
     yield from sieve(filter(lambda x: x % prime != 0, stream))
 
-def integers():
-    n = 2
+def integers_from(n):
     while True:
         yield n
         n += 1
 
 def primes():
-    yield from sieve(integers())
+    yield from sieve(integers_from(2))
+
+def ones():
+    while True:
+        yield 1
+
+def add_streams(s1, s2):
+    for i, j in zip(s1, s2):
+        yield i + j
+
+def other_integers():
+    yield from add_streams(integers_from(0), ones())
+
+def factorial():
+    n = 1
+    for i in integers_from(1):
+        n *= i
+        yield n
+
+def partial_sums(stream):
+    s = 0
+    for i in stream:
+        s += i
+        yield s
 
 if __name__ == '__main__':
     for p in primes():
         print(p)
         if p > 100:
+            break
+
+    for i in other_integers():
+        print(i)
+        if i > 100:
+            break
+
+    for i in partial_sums(integers_from(1)):
+        print(i)
+        if i > 10:
             break
--- scm_code/stream.scm
+++ scm_code/stream.scm
@@ -32,6 +32,39 @@
       (cons-stream (proc (stream-car stream))
                    (lambda () (stream-map proc (stream-cdr stream))))))
 
+(define (ones)
+  (cons-stream 1 ones))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define (fibs
+  (cons-stream 0
+               (cons-stream 1
+                            (add-streams (stream-cdr fibs) fibs)))))
+
+(define (stream-filter pred stream)
+  (cond ((eq? stream '()) '())
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (lambda () (stream-filter pred (stream-cdr stream)))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define (factorials 
+  (cons-stream 1
+               (mul-streams (naturals 1) factorials))))
+
+(define (partial-sums stream)
+  (cons-stream (stream-car stream)
+               (add-streams (stream-cdr stream)
+                            (partial-sums stream))))
+
 ; ;; 使用示例
 ; (define nat-stream (naturals 0))  ; 创建自然数流
 ; (stream-take nat-stream 10)       ; 取出前10个自然数
