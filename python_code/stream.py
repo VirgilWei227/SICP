62caf10a1797590553ba2bb1a9ca757afb6c2130
VirgilWei227 <MF1923077@smail.nju.edu.cn>
2024-06-08 21:45:10 +0800

    0608

--- python_code/stream.py
+++ python_code/stream.py
@@ -34,18 +34,86 @@ def partial_sums(stream):
         s += i
         yield s
 
+def merge(s1, s2):
+    i = next(s1)
+    j = next(s2)
+    while True:
+        if i < j:
+            yield i
+            i = next(s1)
+        elif i > j:
+            yield j
+            j = next(s2)
+        else:
+            yield i
+            i = next(s1)
+            j = next(s2)
+
+def scale_stream(s, factor):
+    return map(lambda x: x * factor, s)
+
+def hamming_numbers():
+    def numbers():
+        yield 1
+        for n in merge(scale_stream(numbers(), 2), merge(scale_stream(numbers(), 3), scale_stream(numbers(), 5))):
+            yield n
+    return numbers()
+
+def integrate_series(s):
+    i = 1
+    for n in s:
+        yield n * 1 / i
+        i += 1
+
+def exp_series():
+    yield 1
+    yield from integrate_series(exp_series())
+
+def sin_series():
+    yield 0
+    yield from integrate_series(cos_series())
+
+def cos_series():
+    yield 1
+    yield from scale_series(integrate_series(sin_series()), -1)
+
 if __name__ == '__main__':
-    for p in primes():
-        print(p)
-        if p > 100:
-            break
+    # for p in primes():
+    #     print(p)
+    #     if p > 100:
+    #         break
 
-    for i in other_integers():
-        print(i)
-        if i > 100:
-            break
+    # for i in other_integers():
+    #     print(i)
+    #     if i > 100:
+    #         break
 
-    for i in partial_sums(integers_from(1)):
+    # for i in partial_sums(integers_from(1)):
+    #     print(i)
+    #     if i > 10:
+    #         break
+
+    # for i in factorial():
+    #     print(i)
+    #     if i > 30:
+    #         break
+
+    # for i in hamming_numbers():
+    #     print(i)
+    #     if i > 100:
+    #         break
+
+    for i in exp_series():
         print(i)
         if i > 10:
             break
+
+    # for i in sin_series():
+    #     print(i)
+    #     if i > 10:
+    #         break
+
+    # for i in cos_series():
+    #     print(i)
+    #     if i > 10:
+    #         break
--- scm_code/stream.scm
+++ scm_code/stream.scm
@@ -35,6 +35,9 @@
 (define (ones)
   (cons-stream 1 ones))
 
+(define (integers)
+  (cons-stream 1 (stream-map (lambda (x) (+ x 1)) integers)))
+
 (define (add-streams s1 s2)
   (stream-map + s1 s2))
 
@@ -56,6 +59,9 @@
 (define (mul-streams s1 s2)
   (stream-map * s1 s2))
 
+(define (div-streams s1 s2)
+  (stream-map / s1 s2))
+
 (define (factorials 
   (cons-stream 1
                (mul-streams (naturals 1) factorials))))
@@ -65,6 +71,33 @@
                (add-streams (stream-cdr stream)
                             (partial-sums stream))))
 
+(define S (cons-stream 1 (merge (scale-stream S 2) (merge (scale-stream S 3) (scale-stream S 5)))))
+
+(define (integrade-series s)
+  (mul-streams s (div-streams ones integers)))
+  
+(define (merge s1 s2)
+  (cond ((eq? s1 '()) s2)
+        ((eq? s2 '()) s1)
+        (else (if (< (stream-car s1) (stream-car s2))
+                  (cons-stream (stream-car s1) (merge (stream-cdr s1) s2))
+                  (cons-stream (stream-car s2) (merge s1 (stream-cdr s2)))))))
+
+(define (exp-series)
+  (cons-stream 1 (integrade-series exp-series)))
+
+(define (cos-series)
+  (cons-stream 1 (integrade-series (scale-stream (stream-cdr sin-series) -1))))
+
+(define (sin-series)
+  (cons-stream 0 (integrade-series cos-series)))
+
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1)
+                  (stream-car s2))
+               (add-streams (mul-streams (stream-cdr s1)
+                                         (stream-cdr s2))
+                            (mul-series s1 s2))))
 ; ;; 使用示例
 ; (define nat-stream (naturals 0))  ; 创建自然数流
 ; (stream-take nat-stream 10)       ; 取出前10个自然数
